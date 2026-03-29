ca3dfa1597a7f858943bbb2f684f611cf10999eb
pierce-wexler <pwexler@vt.edu>
2026-03-29 16:52:01 -0400

    Database connection working

--- phase-5-app/backend/index.js
+++ phase-5-app/backend/index.js
@@ -1,10 +1,34 @@
+// File created/updated with help from chatgpt, as well as canvas video resources
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+app.get("/api/app_users", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM app_user");
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to connect to db" });
+  }
+});
 
 // Test route
 app.get("/api/test", (req, res) => {
